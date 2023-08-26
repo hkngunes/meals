a3dd5c42d8f92ffcaaf138850e41a9aac266bd61
Hakan Güneş <enghakangunes@gmail.com>
2023-08-26 11:59:28 +0300

    Animation with Tween.animate method

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -55,16 +55,13 @@ class _CategoriesScreenState extends State<CategoriesScreen>
     return AnimatedBuilder(
       animation: _animationController,
       builder: (context, child) => SlideTransition(
-        position: _animationController.drive(
-          Tween(
-            begin: const Offset(
-              0,
-              0.3,
-            ),
-            end: const Offset(
-              0,
-              0,
-            ),
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeOut,
           ),
         ),
         child: child,
